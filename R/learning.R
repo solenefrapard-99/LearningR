39c5877ca4c2c942300c686dc13c15b4481feae6
Solene Frapard <solene.frapard@gmail.com>
2023-01-16 14:44:46 +0100

    Initial commit

---
+++ R/learning.R
@@ -0,0 +1,24 @@
+10
+
+# R basics ----------------------------------------------------------------
+
+
+
+weight_kilos <- 100
+weight_kilos <- 10
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+2 + 2
+
+2 + 2
+
+
+# Packages  -----------------------------------------------------------
+
+library(tidyverse) #Tidyverse is a metapackage, collection of packages that works together. Always use, if we can, packages from tidyverse of R Studio.
+
